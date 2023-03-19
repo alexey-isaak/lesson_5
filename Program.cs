d349febe15a292fc30803f9da7ee8cc2e98015d7
Alexey_I <gafar@xaker.ru>
2023-03-19 13:01:09 +0300

    Создание массива.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите ряд строк через запятую");
+Console.WriteLine("Каждая строка может содержать люое количество символов:");
+string[] array = Console.ReadLine()!.Split(',').ToArray();
+
+var str = string.Join(" ", array);
+Console.WriteLine(str);
+
+
