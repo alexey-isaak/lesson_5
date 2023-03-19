7e1a4ad716de70c77e1ea1a3ae2842e2bd63ca0e
Alexey_I <gafar@xaker.ru>
2023-03-19 13:09:34 +0300

    Создание цикла.

--- Program.cs
+++ Program.cs
@@ -5,4 +5,13 @@ string[] array = Console.ReadLine()!.Split(',').ToArray();
 var str = string.Join(" ", array);
 Console.WriteLine(str);
 
-
+int len = array.Length;
+string[] array_element_3 = new string[len];
+for(int i = 0; i < len; i++)
+{
+    int len_element_array = array[i].Length;
+    if (len_element_array <= 3)
+        {
+            array_element_3[i] = array[i];
+        }
+}
